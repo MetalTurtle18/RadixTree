4d711141f7d55e3bc2bc666f73a12a96320851fe
jmskeoch <jmskeoch@yahoo.com>
2021-11-09 12:38:58 -0800

    implement basic trie structure

--- src/RadixTree.java
+++ src/RadixTree.java
@@ -1,94 +1,74 @@
 /**
- * A class to implement a radix tree
- * Names: Dorian, Jason, Nick
+ * A RadixTree, in layman's terms, is a compressed Trie
  */
 public class RadixTree {
+    RadixNode root; //Instance Variables
 
-    public static void main(String[] args) {
-        Trie hi = new Trie();
-        hi.add("please");
-        hi.add("people");
-        hi.add("pleasure");
-
-        hi.print();
-    }
-}
-
-class Trie {
-    TrieNode root;
-
-    public Trie() {
-        root = new TrieNode();
+    //Constructor
+    public RadixTree() {
+        root = new RadixNode();
     }
 
-    public void compress() {
-        int count = 0;
-        for (int i = 0; i < 26; i++) {
-            if (root.children[i] != null) count++;
-        }
-        if (count == 1) {
-            //Compress
-        }
-        
-    }
+    /**
+     * Functional Methods
+     */
 
-    // Inserts a String into the Trie
+    //Wrapper method for the recursive addRec()
     public void add(String str) {
         addRec(str, root);
     }
-
-    private void addRec(String str, TrieNode current) {
+    public void addRec(String str, RadixNode current) {
         if (str == null || str.length() == 0) return;
         var node = current.children[str.charAt(0) - 97];
-        if (node == null) node = new TrieNode();
+        if (node == null) node = new RadixNode();
         node.isWord = str.length() == 1 || node.isWord; // The || is to leave it a word if it already is a word
         current.children[str.charAt(0) - 97] = node;
         addRec(str.substring(1), node);
     }
 
-    // "Deletes" a String from the Trie
-    // In actuality, we will just unmark the String as a word
+    //Wrapper method for the recursive removeRec()
     public void remove(String str) {
         removeRec(str, root);
     }
 
-    private void removeRec(String str, TrieNode current) {
-        var child = current.children[str.charAt(0) - 97];
-        if (child == null) return;
-        if (str.length() == 1) child.isWord = false;
-        else removeRec(str.substring(1), child);
-    }
+    /**
+     * Recursively remove the inputted word from the RadixTree,
+     * Splitting and compressing as need be
+     */
+    public void removeRec(String str, RadixNode current) {}
 
-    // Prints out the Trie
-    // Code is provided
-    public void print() {
-        printRec(this.root, "");
-    }
-
-    // Recursive code that actually prints out the Trie
-    // Code is provided
-    public void printRec(TrieNode current, String str) {
+    public void printRec(RadixNode current, String str) {
         if (current == null)
             return;
-        if (current.isWord)
-            System.out.println(str);
+        System.out.println(str);
         for (char c = 'a'; c <= 'z'; c++)
             printRec(current.children[c - 97], str + c);
     }
 
-    static class TrieNode {
-        TrieNode[] children;
-        String multiNode;
-        boolean isWord;
-        boolean isCompressed;
+    //The main method is to be used for tests on the implementation of a RadixTree
+    public static void main(String[] args) {
+        RadixTree rt = new RadixTree();
+        rt.add("please");
+        rt.add("place");
+        rt.add("pleasure");
 
-        public TrieNode() {
-            children = new TrieNode[26];
-            isWord = false;
-            isCompressed = false;
-            multiNode = "";
-        }
+        rt.printRec(rt.root, "");
     }
 
+}
 
+
+/**
+ * A class for manipulating the nodes within the Radix Tree
+ */
+class RadixNode {
+    RadixNode[] children;
+    String text; // If empty string, represents a single char. Otherwise, represents a group of characters
+    boolean isWord;
+
+    public RadixNode() {
+        children = new RadixNode[26];
+        text = "";
+        isWord = false;
+    }
 }
